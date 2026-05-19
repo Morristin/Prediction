76a698417c19929773280154e7776ecf18faab14
Morristin <takemehands@icloud.com>
2026-05-19 09:41:57 +0800

    feat(dataset): Add normalization to training data
    1. Add subclass TrainingDataSubset to manage price datas and perform normalization.
    2. Final training data will be more flat and implement of Dataset protocol will be more easily.

--- training.py
+++ training.py
@@ -18,33 +18,46 @@ class PriceDataset(Dataset):
 
         :param file: A CSV file which column header contains `name`, `price` and `date`.
         """
-        from collections import defaultdict
 
-        self.training_data = defaultdict(lambda: list())
+        self.training_data: list[tuple[list[float], float]] = list()
         table = open(file, 'r').readlines()
 
         # Process the column header of CSV file.
         column_header = tuple(unit.strip().lower() for unit in table[0].split(','))
-        data_index = dict()
-        for detect_part in ('name', 'price', 'date'):
-            data_index[detect_part] = column_header.index(detect_part)
+        data_index = {detect_part: column_header.index(detect_part) for detect_part in ('name', 'price', 'date')}
 
-        # Convert all data into the format of PyTorch.Tensor.
+        # Convert all data using sliding window method.
+        class TrainingDataSubset:
+            def __init__(self):
+                self.prices: list[float] = list()
+                self._max_price, self._min_price = None, None
+
+            def add(self, price: float):
+                self.prices.append(price)
+                self._max_price = max(price, self._max_price) if self._max_price is not None else price
+                self._min_price = min(price, self._min_price) if self._min_price is not None else price
+
+            def training_data(self, sliding_window_size: int) -> list[tuple[list[float], float]]:
+                """:return: The normalization result of training data in sliding window format."""
+
+                self.prices = [(price - self._min_price) / (self._max_price - self._min_price) for price in self.prices]
+                return [
+                    (self.prices[index - sliding_window_size - 1 : -1], self.prices[-1])
+                    for index in range(sliding_window_size, len(self.prices))
+                ]
+
         # Special treat the first line of data. Make sure arguments are assigned before used.
         name: str = table[1][data_index['name']]
-        x: list[float] = list()
+        data_subset: TrainingDataSubset = TrainingDataSubset()
         for data in table[1:]:
             data = tuple(unit.strip() for unit in data.split(','))
 
             if data[data_index['name']] == name:
                 # TODO: Check the date and create lack data using linear interpolation.
-                x.append(float(data[data_index['price']]))
-                if len(x) > self.SLIDING_WINDOW_SIZE:
-                    self.training_data[name].append((x[-self.SLIDING_WINDOW_SIZE - 1 : -1], x[-1]))
+                data_subset.add(float(data[data_index['price']]))
             else:
-                # Reinitialize the arguments.
-                name = data[data_index['name']]
-                x = [float(data[data_index['price']])]
+                self.training_data += data_subset.training_data(sliding_window_size=self.SLIDING_WINDOW_SIZE)
+                name, data_subset = data[data_index['name']], TrainingDataSubset()
 
 
 class TorchTrainer:

5dee7f2e6be22c6b4e640c6b2d71d641b90bbc36
Morristin <takemehands@icloud.com>
2026-05-19 10:51:40 +0800

    feat(dataset): Now dataset is split into training data and test data
    1. Program now read data using `load_data` function in TorchTrainer, and PriceDataset only exists for satisfying the Dataset protocol.
    2. Dataset is now split by specific percentage, which aims for future data process.

--- training.py
+++ training.py
@@ -7,9 +7,34 @@ logging.getLogger(__name__)
 
 
 class PriceDataset(Dataset):
-    SLIDING_WINDOW_SIZE = 30
+    def __init__(self, data):
+        self.data = data
 
-    def __init__(self, file: str):
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index) -> tuple[torch.Tensor, torch.Tensor]:
+        x, y = self.data[index]
+        return torch.tensor(x, dtype=torch.float32), torch.tensor(y, dtype=torch.float32)
+
+
+class TorchTrainer:
+    SLIDING_WINDOW_SIZE = 30
+    TRAINING_AND_TEST_SPLIT_POINT = 0.8
+
+    # noinspection PyUnresolvedReferences
+    def __init__(self):
+        accelerator = torch.accelerator.current_accelerator()
+        if accelerator is not None:
+            logging.info(f'Detect accelerator {accelerator.type}. Use {accelerator.type} for training.')
+            self.device: torch.device = torch.device(accelerator.type)
+        else:
+            logging.warning('Can not detect any accelerator, use CPU to train model.')
+            self.device: torch.device = torch.device('cpu')
+
+        self.training_data, self.test_data = None, None
+
+    def load_data(self, file: str, /, sliding_window_size: int = SLIDING_WINDOW_SIZE):
         """
         Detect and calculate tensor data from CSV file.
 
@@ -17,9 +42,11 @@ class PriceDataset(Dataset):
         Callers need to handle exceptions themselves or make sure source file is valid.
 
         :param file: A CSV file which column header contains `name`, `price` and `date`.
+        :param sliding_window_size: The size of sliding window when split data into units.
         """
 
-        self.training_data: list[tuple[list[float], float]] = list()
+        training_data: list[tuple[list[float], float]] = list()
+        test_data: list[tuple[list[float], float]] = list()
         table = open(file, 'r').readlines()
 
         # Process the column header of CSV file.
@@ -32,19 +59,18 @@ class PriceDataset(Dataset):
                 self.prices: list[float] = list()
                 self._max_price, self._min_price = None, None
 
+            def __len__(self):
+                return len(self.prices)
+
             def add(self, price: float):
                 self.prices.append(price)
                 self._max_price = max(price, self._max_price) if self._max_price is not None else price
                 self._min_price = min(price, self._min_price) if self._min_price is not None else price
 
-            def training_data(self, sliding_window_size: int) -> list[tuple[list[float], float]]:
+            def normalization_data(self, window_size: int) -> list[tuple[list[float], float]]:
                 """:return: The normalization result of training data in sliding window format."""
-
-                self.prices = [(price - self._min_price) / (self._max_price - self._min_price) for price in self.prices]
-                return [
-                    (self.prices[index - sliding_window_size - 1 : -1], self.prices[-1])
-                    for index in range(sliding_window_size, len(self.prices))
-                ]
+                prices = [(price - self._min_price) / (self._max_price - self._min_price) for price in self.prices]
+                return [(prices[index - window_size - 1 : -1], prices[-1]) for index in range(window_size, len(prices))]
 
         # Special treat the first line of data. Make sure arguments are assigned before used.
         name: str = table[1][data_index['name']]
@@ -56,24 +82,13 @@ class PriceDataset(Dataset):
                 # TODO: Check the date and create lack data using linear interpolation.
                 data_subset.add(float(data[data_index['price']]))
             else:
-                self.training_data += data_subset.training_data(sliding_window_size=self.SLIDING_WINDOW_SIZE)
+                # Calculate split point to split training data and test data.
+                split_point: int = int(len(data_subset) * self.TRAINING_AND_TEST_SPLIT_POINT)
+                normalization_data = data_subset.normalization_data(sliding_window_size)
+                training_data += normalization_data[:split_point]
+                test_data += normalization_data[split_point:]
+
+                # Reinitialize the arguments for next loop.
                 name, data_subset = data[data_index['name']], TrainingDataSubset()
 
-    def __len__(self):
-        return len(self.training_data)
-
-    def __getitem__(self, index) -> tuple[torch.Tensor, torch.Tensor]:
-        x, y = self.training_data[index]
-        return torch.tensor(x, dtype=torch.float32), torch.tensor(y, dtype=torch.float32)
-
-
-class TorchTrainer:
-    # noinspection PyUnresolvedReferences
-    def __init__(self):
-        accelerator = torch.accelerator.current_accelerator()
-        if accelerator is not None:
-            logging.info(f'Detect accelerator {accelerator.type}. Use {accelerator.type} for training.')
-            self.device: torch.device = torch.device(accelerator.type)
-        else:
-            logging.warning('Can not detect any accelerator, use CPU to train model.')
-            self.device: torch.device = torch.device('cpu')
+        self.training_data, self.test_data = PriceDataset(training_data), PriceDataset(test_data)
